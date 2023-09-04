c7be45d5d675af2eb0dc4ce7af47aa09a6ae8766
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-09-04 15:34:45 -0300

    Update listaExercicios01.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,5 +1,1 @@
 SELECT titulo FROM livros;
-
-SELECT nome FROM autores WHERE YEAR(nascimento) < 1900;
-
-SELECT livros.titulo FROM livros INNER JOIN autores ON livros.autor_id = autores.id WHERE autores.nome = 'J.K. Rowling';
