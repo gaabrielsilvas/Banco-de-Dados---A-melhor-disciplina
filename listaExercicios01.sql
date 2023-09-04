cd36a37103e994e4518cca0666141b08acdf4000
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-09-04 15:48:46 -0300

    Update listaExercicios13.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -22,3 +22,4 @@ SELECT livros.titulo AS livro, autores.nome AS autor FROM livros INNER JOIN auto
 
 SELECT alunos.nome AS aluno, matriculas.curso FROM alunos LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id;
 
+SELECT autores.nome AS autor, COALESCE(livros.titulo, 'Nenhum livro publicado') AS livro FROM autores LEFT JOIN livros ON autores.id = livros.autor_id;
