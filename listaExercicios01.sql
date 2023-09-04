0125b4decd14542b10818460483775e779a8a3fe
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-09-04 15:38:04 -0300

    Update listaExercicios04.sql

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -3,3 +3,5 @@ SELECT titulo FROM livros;
 SELECT nome FROM autores WHERE YEAR(nascimento) < 1900;
 
 SELECT livros.titulo FROM livros INNER JOIN autores ON livros.autor_id = autores.id WHERE autores.nome = 'J.K. Rowling';
+
+SELECT alunos.nome FROM alunos INNER JOIN matriculas ON alunos.id = matriculas.aluno_id WHERE matriculas.curso = 'Engenharia de Software';
