fc50ff28143fb3aad724378d059d661282e2dddd
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-09-26 21:47:51 -0300

    Update listaExercícios02.sql
    exercício 6

--- listaExercícios02.sql
+++ listaExercícios02.sql
@@ -39,4 +39,10 @@ END;
 
 //
 
+--exercicio 6
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN SELECT Livro.Titulo FROM Livro INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID WHERE Categoria.Nome = categoriaNome;
+END;
+
+//
 
