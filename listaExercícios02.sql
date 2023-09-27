d4570f4ab69720a212fa2583c9f6a2b51a76488f
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-09-26 21:44:01 -0300

    Update listaExercícios02.sql
    exercício 3

--- listaExercícios02.sql
+++ listaExercícios02.sql
@@ -11,3 +11,9 @@ CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
 BEGIN SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome FROM Livro INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID WHERE Categoria.Nome = categoriaNome;
 END;
 //
+
+--exercicio 3
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100), OUT totalLivros INT)
+BEGIN SELECT COUNT(*) INTO totalLivros FROM Livro INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID WHERE Categoria.Nome = categoriaNome;
+END;
+//
