477f7048e6263a5aa8d048dd7c2e620b4b32c943
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-10-02 18:21:01 -0300

    Create listaExercícios03.sql
    1) Exercício A

---
+++ listaExercícios03.sql
@@ -0,0 +1,10 @@
+1) a) 
+  
+CREATE TABLE nomes (
+  nome VARCHAR(50)
+);
+
+insert into nomes (nome) values ('Roberta');
+insert into nomes (nome) values ('Roberto');
+insert into nomes (nome) values ('Maria Clara');
+insert into nomes (nome) values ('João');
