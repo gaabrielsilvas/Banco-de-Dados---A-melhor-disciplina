c2678af71aa8b4a18cee50855fa642924a257fd2
gaabrielsilvas <102781306+gaabrielsilvas@users.noreply.github.com>
2023-10-25 21:16:18 -0300

    Update exerciciosDeFixacao20231025.sql
    Exercício 2

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -6,3 +6,12 @@ begin
 	insert into Auditoria (mensagem, data_hora)
     values ('Novo cliente inserido', NOW() );
 END;
+
+--exercicio 2--
+create trigger BeforeDeleteCliente
+before delete
+on Clientes for each row
+begin
+	insert into Auditoria (mensagem, data_hora)
+    values ('Tentativa de exclusão do cliente ID:  ', OLD.cliente_id, NOW() );
+end;
